67f4a299704c06a038b399d10ec0f72810f4f065
livelyPeng <32978177+livelyPeng@users.noreply.github.com>
2020-07-20 10:25:01 +0800

    Update build-entry.js

--- build/build-entry.js
+++ build/build-entry.js
@@ -35,7 +35,7 @@ const install = function(Vue, opts = {}) {
     Vue.component(component.name, component);
   });
 
-  Vue.prototype.$ELEMENT = {
+  Vue.prototype.$UMYUI = {
     size: opts.size || '',
     zIndex: opts.zIndex || 2000
   };
