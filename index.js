7625a3482bc448be1cc030867bd9a3018aef5a15
livelyPeng <32978177+livelyPeng@users.noreply.github.com>
2020-07-20 10:24:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const install = function(Vue, opts = {}) {
     Vue.component(component.name, component);
   });
 
-  Vue.prototype.$ELEMENT = {
+  Vue.prototype.$UMYUI = {
     size: opts.size || '',
     zIndex: opts.zIndex || 2000
   };
