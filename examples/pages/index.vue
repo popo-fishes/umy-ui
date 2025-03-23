87e52eb40fe18007097c13d7697b56fb9c793180
PL196\40380 <403802162@qq.com>
2025-03-23 10:01:24 +0800

    fix: 更新文档

--- examples/pages/index.vue
+++ examples/pages/index.vue
@@ -2,53 +2,58 @@
   <div class="homeIndex">
     <div class="desc width100">
       <h1>网站快速成型工具</h1>
-      <h2 class="desc-text">为开发者准备的基于 Vue 2.0 的桌面端组件库; 流畅渲染表格万级数据</h2>
-      <img src="../assets/banner.png" class="banner">
+      <h2 class="desc-text">
+        为开发者准备的基于 Vue 2.0 的桌面端组件库; 流畅渲染表格万级数据
+      </h2>
+      <img src="../assets/banner.png" class="banner" />
     </div>
     <div class="shiyong">
       <p class="card-title2">国内使用umy-ui的地区分布图</p>
-      <p class="miaoshu">umy-ui完美解决表格数据多而导致卡问题，目前使用的人分布在全国各地及台湾。其中可以看出重要的IT城市，如北京，上海，深圳，广州，浙江，成都，武汉，南京等都有用户在使用！</p>
-      <img src="../assets/umyuifenbu.png">
+      <p class="miaoshu">
+        umy-ui完美解决表格数据多而导致卡问题，目前使用的人分布在全国各地及台湾。其中可以看出重要的IT城市，如北京，上海，深圳，广州，浙江，成都，武汉，南京等都有用户在使用！
+      </p>
+      <img src="../assets/umyuifenbu.png" />
     </div>
     <div class="waa desc">
       <p class="card-title">四大利器，为您保驾护航</p>
       <ul class="row">
         <li class="col-md-3">
-          <div class="card-item"><img src="https://uviewui.com/index/guide.png">
+          <div class="card-item">
+            <img src="https://uviewui.com/index/guide.png" />
             <h3>文档</h3>
-            <p>涵盖umy-ui各个方面，给开发者方向指导和设计理念，让您茅塞顿开，一马平川 </p>
+            <p>
+              涵盖umy-ui各个方面，给开发者方向指导和设计理念，让您茅塞顿开，一马平川
+            </p>
           </div>
         </li>
         <li class="col-md-3">
-          <div class="card-item"><img src="https://uviewui.com/index/components.png">
+          <div class="card-item">
+            <img src="https://uviewui.com/index/components.png" />
             <h3>组件</h3>
-            <p>众多组件覆盖开发过程的各个需求，组件功能丰富，u-table组件解决表格万级数据渲染卡顿问题。</p>
+            <p>
+              众多组件覆盖开发过程的各个需求，组件功能丰富，u-table组件解决表格万级数据渲染卡顿问题。
+            </p>
           </div>
         </li>
         <li class="col-md-3">
-          <div class="card-item"><img src="https://uviewui.com/index/js.png">
+          <div class="card-item">
+            <img src="https://uviewui.com/index/js.png" />
             <h3>工具库</h3>
-            <p>众多的贴心小工具，是您开发过程中召之即来的利器，让您飞镖在手，百步穿杨</p>
+            <p>
+              众多的贴心小工具，是您开发过程中召之即来的利器，让您飞镖在手，百步穿杨
+            </p>
           </div>
         </li>
         <li class="col-md-3">
-          <div class="card-item"><img src="https://uviewui.com/index/layout.png">
+          <div class="card-item">
+            <img src="https://uviewui.com/index/layout.png" />
             <h3>布局</h3>
-            <p>收集众多的常用页面和布局，减少开发者的重复工作，让您专注逻辑，事半功倍</p>
+            <p>
+              收集众多的常用页面和布局，减少开发者的重复工作，让您专注逻辑，事半功倍
+            </p>
           </div>
         </li>
       </ul>
-    </div>
-    <div class="width100 desc">
-      <p class="card-title">友情链接</p>
-      <div class="row overflowHidden">
-        <div class="link__wrap fl" v-for="(item, index) in LinksData" :key="index">
-          <a :href="item.url" target="_blank">
-            <img v-if="item.img" :src="item.img">
-            <div class="texts" v-if="item.title">{{ item.title }}</div>
-          </a>
-        </div>
-      </div>
     </div>
     <div class="guanjianzi">
       <p class="card-title">您可搜索如下关键字，即可找到umy-ui</p>
@@ -72,146 +77,129 @@
       <h4>表格数据多了就渲染慢</h4>
       <h4>表格数据多了就卡顿</h4>
     </div>
-    <p style="padding: 100px 0 30px 0;text-align: center;color: #606266;font-size: 14px">Copyright 2020 UmyUi |
-      <a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank" style="color: #606266;">蜀ICP备2020025971号-1</a></p>
+    <p
+      style="padding: 100px 0 30px 0;text-align: center;color: #606266;font-size: 14px"
+    >
+      Copyright 2020 UmyUi |
+      <a
+        href="https://beian.miit.gov.cn/#/Integrated/index"
+        target="_blank"
+        style="color: #606266;"
+        >蜀ICP备2020025971号-1</a
+      >
+    </p>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "homeIndex",
-    data () {
-      return {
-        LinksData: [
-          {
-            url: 'http://beautiful.panm.cn/vue-admin-beautiful/#/index',
-            img: require('../assets/vue-admin-beautiful.png')
-          },
-          {
-            url: 'https://uviewui.com/',
-            img: require('../assets/uview2.png')
-          },
-          {
-            url: 'https://github.com/JakHuang/form-generator',
-            img: require('../assets/from-gen-all.jpg')
-          }
-        ]
-      }
-    }
+export default {
+  name: "homeIndex",
+  data() {
+    return {};
   }
+};
 </script>
 
 <style scoped lang="scss">
-  .homeIndex {padding-top: 50px;max-width: 1200px;margin: 0 auto;
-    .shiyong {width: 100%;text-align: center;
-      .card-title2 {
-        color: #303133;
-        font-size: 24px;
-        padding: 50px 0 20px 0;
-      }
-      .miaoshu {
-        margin: 0 auto;
-        font-size: 14px;
-        color: #5e6d82;
-        line-height: 2em;
-        width: 80%;
-        margin-bottom: 30px;
-      }
-      img {width: 750px;}
-    }
-    .guanjianzi {
-      text-align: center;
-      padding: 50px 0;
-      h4 {
-        display: inline-block !important;
-        margin-right: 30px;
-        color: #606266;
-        font-weight: inherit;
-      }
-    }
-    .waa {
-      overflow: hidden;
-    }
-    .row {
-      margin-right: -15px;
-      margin-left: -15px;
-      overflow: hidden;
-      .col-md-3 {
-        width: 25%;
-        float: left;
-        padding: 0 20px;
-        box-sizing: border-box;
-        .card-item {
-          height: 390px;
-          border: 1px solid #eaeefb;
-          border-radius: 5px;
-          -webkit-transition: bottom .4s;
-          transition: bottom .4s;
-          position: relative;
-          bottom: 0;
-          img {
-            width: 160px;
-            margin: 40px auto 20px;
-          }
-          h3 {
-            color: #606266;
-            font-size: 18px;
-            font-weight: 400;
-            line-height: 45px;
-          }
-          p {
-            font-size: 14px;
-            color: #909399;
-            padding: 0 25px;
-            margin-bottom: 40px;
-          }
-        }
-      }
-    }
-    .desc {text-align: center;
-      h1 {font-weight: 700;color: #303133;}
-      .desc-text {
-        color: #606266;
-        font-size: 16px;
-        border: none;
-        line-height: 50px;
-        font-weight: 400;
-      }
-      .banner {width: 100%;}
-    }
-    .card-title {
+.homeIndex {
+  padding-top: 50px;
+  max-width: 1200px;
+  margin: 0 auto;
+  .shiyong {
+    width: 100%;
+    text-align: center;
+    .card-title2 {
       color: #303133;
       font-size: 24px;
-      padding: 50px 0;
+      padding: 50px 0 20px 0;
+    }
+    .miaoshu {
+      margin: 0 auto;
+      font-size: 14px;
+      color: #5e6d82;
+      line-height: 2em;
+      width: 80%;
       margin-bottom: 30px;
     }
-    .row {
-      margin-right: -15px;
-      margin-left: -15px;
-      .link__wrap {
-        width: 33.33%;
-        padding-right: 15px;
-        padding-left: 15px;
-        margin-bottom: 10px;
-        a {
-          border-radius: 5px;
-          overflow: hidden;
-          display: inline-block;
-        }
+    img {
+      width: 750px;
+    }
+  }
+  .guanjianzi {
+    text-align: center;
+    padding: 50px 0;
+    h4 {
+      display: inline-block !important;
+      margin-right: 30px;
+      color: #606266;
+      font-weight: inherit;
+    }
+  }
+  .waa {
+    overflow: hidden;
+  }
+  .row {
+    margin-right: -15px;
+    margin-left: -15px;
+    overflow: hidden;
+    .col-md-3 {
+      width: 25%;
+      float: left;
+      padding: 0 20px;
+      box-sizing: border-box;
+      .card-item {
+        height: 390px;
+        border: 1px solid #eaeefb;
+        border-radius: 5px;
+        -webkit-transition: bottom 0.4s;
+        transition: bottom 0.4s;
+        position: relative;
+        bottom: 0;
         img {
-          width: 100%;
-          display: block;
+          width: 160px;
+          margin: 40px auto 20px;
         }
-        .texts {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          color: #000c17;
-          &:hover{
-            color: $dh-color;
-          }
+        h3 {
+          color: #606266;
+          font-size: 18px;
+          font-weight: 400;
+          line-height: 45px;
+        }
+        p {
+          font-size: 14px;
+          color: #909399;
+          padding: 0 25px;
+          margin-bottom: 40px;
         }
       }
     }
   }
+  .desc {
+    text-align: center;
+    h1 {
+      font-weight: 700;
+      color: #303133;
+    }
+    .desc-text {
+      color: #606266;
+      font-size: 16px;
+      border: none;
+      line-height: 50px;
+      font-weight: 400;
+    }
+    .banner {
+      width: 100%;
+    }
+  }
+  .card-title {
+    color: #303133;
+    font-size: 24px;
+    padding: 50px 0;
+    margin-bottom: 30px;
+  }
+  .row {
+    margin-right: -15px;
+    margin-left: -15px;
+  }
+}
 </style>
--- examples/pages/umycomponent.vue
+++ examples/pages/umycomponent.vue
@@ -1,220 +1,248 @@
 <template>
-    <div class="page-component width100 height100">
-        <el-scrollbar class="page-component__nav">
-            <div class="jump-link">
-                <!--<el-button plain @click="dialogVisible = true">成为赞助商</el-button>-->
-                <a href="http://www.vueshop.com.cn/index.php?http_referer=umyui" target="_blank">
-                    <img src="http://uviewui.com/customer/sdf1516355we0sd2.jpg">
-                </a>
-            </div>
-            <p class="title-pl beyond"
-               :class="{ active: activeUrl === basics + 'updateLog' }"
-               @click="navLink({url: 'updateLog'})"
-               style="margin-top: 15px;margin-bottom: -5px;cursor: pointer">更新日志</p>
-            <!--组件列表-->
-            <div class="menuList">
-                <div class="item-pl" v-for="(item, index) in componentsData" :key="index">
-                    <p class="title-pl beyond" :title="item.title">
-                        <!--title为起步或者组件，则不能收起隐藏-->
-                        <span v-if="item.title !== '起步' && item.title !== '组件'" @click="openCom(item.show, index)" class="icon-us user-select">
-                             {{ item.title }}
-                             <span class="u-table-word-icon">
-                                 <i :class="[item.show ? 'el-icon-arrow-up' : 'el-icon-arrow-down']"></i>
-                             </span>
-                        </span>
-                        <span v-else>{{ item.title }}</span>
-                    </p>
-                    <ul class="list-pl" v-if="item.show">
-                        <li class="nav-item-c"
-                            v-for="(child, inx) in item.list"
-                            @click="navLink(child)"
-                            :title="child.title"
-                            :class="{ active: activeUrl === basics + child.url }"
-                            :key="inx">
-                            {{ child.title }}
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </el-scrollbar>
-        <div class="page-component__content">
-            <nuxt-child class="content" :class="{ 'customMdclass': mdStyle }"/>
+  <div class="page-component width100 height100">
+    <el-scrollbar class="page-component__nav">
+      <div class="jump-link">
+        <a
+          v-for="(item, index) in LinksData"
+          :key="index"
+          :href="item.url"
+          target="_blank"
+        >
+          <img :src="item.img" />
+        </a>
+      </div>
+      <p
+        class="title-pl beyond"
+        :class="{ active: activeUrl === basics + 'updateLog' }"
+        @click="navLink({ url: 'updateLog' })"
+        style="margin-top: 15px;margin-bottom: -5px;cursor: pointer"
+      >
+        更新日志
+      </p>
+      <!--组件列表-->
+      <div class="menuList">
+        <div
+          class="item-pl"
+          v-for="(item, index) in componentsData"
+          :key="index"
+        >
+          <p class="title-pl beyond" :title="item.title">
+            <!--title为起步或者组件，则不能收起隐藏-->
+            <span
+              v-if="item.title !== '起步' && item.title !== '组件'"
+              @click="openCom(item.show, index)"
+              class="icon-us user-select"
+            >
+              {{ item.title }}
+              <span class="u-table-word-icon">
+                <i
+                  :class="[
+                    item.show ? 'el-icon-arrow-up' : 'el-icon-arrow-down'
+                  ]"
+                ></i>
+              </span>
+            </span>
+            <span v-else>{{ item.title }}</span>
+          </p>
+          <ul class="list-pl" v-if="item.show">
+            <li
+              class="nav-item-c"
+              v-for="(child, inx) in item.list"
+              @click="navLink(child)"
+              :title="child.title"
+              :class="{ active: activeUrl === basics + child.url }"
+              :key="inx"
+            >
+              {{ child.title }}
+            </li>
+          </ul>
         </div>
-      <el-dialog
-        title="成为赞助商"
-        :visible.sync="dialogVisible"
-        width="40%"
-        :before-close="handleClose">
-        <div>
-          <p>如果您有品牌推广、活动推广、招聘推广、社区合作等需求，欢迎联系我们，成为赞助商。</p>
-          <p>您的广告将出现在umy-ui文档所有子页面及 GitHub Readme 等页面。</p>
-          <p>咨询QQ：403802162, 咨询邮箱:403802162@qq.com, 咨询微信号：travel196</p>
-        </div>
-        <span slot="footer" class="dialog-footer">
-          <el-button @click="dialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
-        </span>
-      </el-dialog>
-      <el-dialog
-        title="提示"
-        :visible.sync="tishi"
-        width="30%"
-        :before-close="() => tishi = false">
-        <span>二群已经被封了，建议有问题加3群：127993351</span>
-        <span slot="footer" class="dialog-footer">
-          <el-button @click="tishi = false">取 消</el-button>
-          <el-button type="primary" @click="tishi = false">确 定</el-button>
-        </span>
-      </el-dialog>
+      </div>
+    </el-scrollbar>
+    <div class="page-component__content">
+      <nuxt-child class="content" :class="{ customMdclass: mdStyle }" />
     </div>
+    <el-dialog
+      title="提示"
+      :visible.sync="tishi"
+      width="30%"
+      :before-close="() => (tishi = false)"
+    >
+      <span>二群已经被封了，建议有问题加3群：127993351</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="tishi = false">取 消</el-button>
+        <el-button type="primary" @click="tishi = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-    import tableMenuData from '../utils/table-menu-data'
-    import umyuiMenuData from '../utils/umy-ui-component'
-    // 二级固定 /wel
-    export default {
-        name: "umycomponent",
-        data () {
-            return {
-                tishi: false,
-                dialogVisible: false,
-                basics: '/umycomponent/',
-                // show代表默认展开与否
-                componentsData: [
-                    {
-                        title: '起步',
-                        show: true,
-                        list: [
-                            {title: '介绍', url: 'intro'},
-                            {title: '安装', url: 'installation'},
-                            {title: '快速上手', url: 'quickstart'},
-                            {title: '国际化', url: 'i18n'},
-                            {title: '用前须知', url: 'noticeBefore'}
-                        ]
-                    },
-                    ...umyuiMenuData,
-                    ...tableMenuData
-                ]
-            }
+import tableMenuData from "../utils/table-menu-data";
+import umyuiMenuData from "../utils/umy-ui-component";
+// 二级固定 /wel
+export default {
+  name: "umycomponent",
+  data() {
+    return {
+      tishi: false,
+      basics: "/umycomponent/",
+      LinksData: [
+        {
+          url: "https://www.jnpfsoft.com/index.html?from=umyui",
+          img: require("../assets/ad1.png")
         },
-        computed:{
-            activeUrl () {
-               let str = this.$store.state.url
-               if (str.charAt(str.length - 1) === '/') {
-                 return str.substring(0, str.length - 1)
-               } else {
-                 return this.$store.state.url
-               }
-            },
-            // 判断是否为自定义的md api页面，然后写不同的样式
-            mdStyle () {
-                const url = this.$store.state.url
-                return url === '/umycomponent/u-table-column-api' ||
-                    url === '/umycomponent/paginationAndSideslipFrame' ||
-                    url === '/umycomponent/ubutton'
-            },
+        {
+          url: "https://www.crmeb.com/?from=umyui",
+          img: require("../assets/ad2.jpeg")
         },
-        mounted () {
-            this.componentsData.forEach(row => {
-                row.list.forEach(item => {
-                    if (this.basics + item.url === this.activeUrl) {
-                        row.show = true
-                    }
-                })
-            })
-        },
-        methods: {
-            openCom (fals, index) {
-              this.componentsData[index].show = !fals
-            },
-            handleClose () {
-               this.dialogVisible = false
-            },
-            navLink (row) {
-                if (row.url) {
-                    this.$router.push({
-                        path: this.basics + row.url
-                    })
-                }
-            }
+        {
+          url: "https://www.yisu.com/cdn/huodong.html?f=umyui",
+          img: require("../assets/ad3.jpeg")
         }
+      ],
+      // show代表默认展开与否
+      componentsData: [
+        {
+          title: "起步",
+          show: true,
+          list: [
+            { title: "介绍", url: "intro" },
+            { title: "安装", url: "installation" },
+            { title: "快速上手", url: "quickstart" },
+            { title: "国际化", url: "i18n" },
+            { title: "用前须知", url: "noticeBefore" }
+          ]
+        },
+        ...umyuiMenuData,
+        ...tableMenuData
+      ]
+    };
+  },
+  computed: {
+    activeUrl() {
+      let str = this.$store.state.url;
+      if (str.charAt(str.length - 1) === "/") {
+        return str.substring(0, str.length - 1);
+      } else {
+        return this.$store.state.url;
+      }
+    },
+    // 判断是否为自定义的md api页面，然后写不同的样式
+    mdStyle() {
+      const url = this.$store.state.url;
+      return (
+        url === "/umycomponent/u-table-column-api" ||
+        url === "/umycomponent/paginationAndSideslipFrame" ||
+        url === "/umycomponent/ubutton"
+      );
     }
+  },
+  mounted() {
+    this.componentsData.forEach(row => {
+      row.list.forEach(item => {
+        if (this.basics + item.url === this.activeUrl) {
+          row.show = true;
+        }
+      });
+    });
+  },
+  methods: {
+    openCom(fals, index) {
+      this.componentsData[index].show = !fals;
+    },
+    navLink(row) {
+      if (row.url) {
+        this.$router.push({
+          path: this.basics + row.url
+        });
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss">
- .page-component{
-     .page-component__nav {
-        width: 250px;
-        padding-left: 30px;
-        z-index: 100;
-        left: 0;
-        background-color: white;
-        border-right: 1px solid #eaecef;
-        position: fixed;
-        top: 57px;
-        height: calc(100% - 66px);
-        bottom: 0;
-        transition: padding-top .3s;
+.page-component {
+  .page-component__nav {
+    width: 250px;
+    padding-left: 30px;
+    z-index: 100;
+    left: 0;
+    background-color: white;
+    border-right: 1px solid #eaecef;
+    position: fixed;
+    top: 57px;
+    height: calc(100% - 66px);
+    bottom: 0;
+    transition: padding-top 0.3s;
 
-        .jump-link {
-            /*background-color: #ebebeb;*/
-            width: 190px;
-            margin-top: 20px;
-            height: 90px;
-            line-height: 90px;
-            border-radius: 5px;
-            overflow: hidden;
-            .el-button {
-              color: $dh-color !important;
-              border: 1px solid $dh-color !important;
-            }
-            a{display: block;}
-            img {width: 100%;height: 100%;}
-        }
+    .jump-link {
+      /*background-color: #ebebeb;*/
+      width: 100%;
+      margin-top: 20px;
+      overflow: hidden;
+
+      a {
+        display: block;
+        margin-bottom: 8px;
+        height: 60px;
+        width: 196px;
+        border-radius: 8px;
+        overflow: hidden;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+        vertical-align: inherit;
+      }
     }
-     .page-component__content {
-         padding-left: 260px;
-         padding-bottom: 100px;
-         min-width: 1200px;
-         max-width: 1400px;
-         overflow-x: auto;
-         box-sizing: border-box;
-         .content {padding-top: 30px;}
-     }
-     .title-pl {
-         font-size: 16px;
-         color: #333;
-         font-weight: 700;
-         line-height: 40px;
-         height: 40px;
-     }
-     .active {
-         color: $dh-color;
-     }
-     .menuList {padding-top: 10px;
-         .list-pl {
-          .nav-item-c {
-              display: block;
-              height: 36px;
-              color: #444;
-              line-height: 36px;
-              font-size: 14px;
-              overflow: hidden;
-              white-space: nowrap;
-              text-overflow: ellipsis;
-              font-weight: 400;
-              padding: 0;
-              border: none!important;
-              &:hover{
-                  color: $dh-color;
-                  cursor: pointer;
-              }
-          }
-          .active {
-              color: $dh-color;
-          }
+  }
+  .page-component__content {
+    padding-left: 260px;
+    padding-bottom: 100px;
+    min-width: 1200px;
+    max-width: 1400px;
+    overflow-x: auto;
+    box-sizing: border-box;
+    .content {
+      padding-top: 30px;
+    }
+  }
+  .title-pl {
+    font-size: 16px;
+    color: #333;
+    font-weight: 700;
+    line-height: 40px;
+    height: 40px;
+  }
+  .active {
+    color: $dh-color;
+  }
+  .menuList {
+    padding-top: 10px;
+    .list-pl {
+      .nav-item-c {
+        display: block;
+        height: 36px;
+        color: #444;
+        line-height: 36px;
+        font-size: 14px;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        font-weight: 400;
+        padding: 0;
+        border: none !important;
+        &:hover {
+          color: $dh-color;
+          cursor: pointer;
         }
-     }
- }
+      }
+      .active {
+        color: $dh-color;
+      }
+    }
+  }
+}
 </style>
